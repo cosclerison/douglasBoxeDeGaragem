52f0242062be8d21bc611227d57ffb4d9ea7466b
Clerison <cosclerison@gmail.com>
2023-07-14 23:04:54 -0300

    Corrigindo resposividade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { VideosComponent } from './videos/videos.component';
+import { FotosComponent } from './fotos/fotos.component';
+import { ContatosComponent } from './contatos/contatos.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SobreComponent,
+    VideosComponent,
+    FotosComponent,
+    ContatosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
